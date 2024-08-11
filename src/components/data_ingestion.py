d35642080864907a44603ef0672c5c85abafaa50
unknown <chatterjeesaurabh38@gmail.com>
2024-08-12 01:14:28 +0530

    components and pipeline updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,19 +12,47 @@ from pathlib import Path
 
 @dataclass
 class DataIngestionConfig:
-    pass
+    raw_data_path:str = os.path.join("artifacts", "raw.csv")
+    train_data_path:str = os.path.join("artifacts", "train.csv")
+    test_data_path:str = os.path.join("artifacts", "test.csv")
 
 
 class DataIngestion:
     def __init__(self):
-        pass
+        self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
+        logging.info("Data Ingestion started")
         try:
-            pass
+            data = pd.read_csv("data/raw.csv")      # load raw data
+            logging.info("reading data")
+
+            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)), exist_ok=True)      # make the artifact folder
+            data.to_csv(self.ingestion_config.raw_data_path, index=False)       # save the raw data to artifact folder
+            logging.info("saved the raw dataset in artifact folder")
+            
+            logging.info("starting train-test split")
+            
+            train_data, test_data = train_test_split(data, test_size=0.25)      # train-test split on the raw data
+            logging.info("train test split completed")
+            
+            train_data.to_csv(self.ingestion_config.train_data_path, index=False)   # save train data to artifact folder
+            test_data.to_csv(self.ingestion_config.test_data_path, index=False)     # save test data to artifact folder
+            
+            logging.info("Data Ingestion completed")
+            
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
         except Exception as e:
             logging.info()
             raise customException(e, sys)
 
 
 
+
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -28,14 +28,14 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
         for i in range(len(models)):
             model = list(models.values())[i]
             # Train model
-            model.fit(X_train,y_train)
+            model.fit(X_train, y_train)
 
             # Predict Testing data
-            y_test_pred =model.predict(X_test)
+            y_test_pred = model.predict(X_test)
 
             # Get R2 scores for train and test data
             #train_model_score = r2_score(ytrain,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] =  test_model_score
 
